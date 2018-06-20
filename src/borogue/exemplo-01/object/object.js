694707c092b5f49930d69065cd266db576803134
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-20 05:21:33 -0400

    Criando Classe em JavaScript, para referenciar um objeto.

--- src/borogue/exemplo-01/object/object.js
+++ src/borogue/exemplo-01/object/object.js
@@ -6,4 +6,14 @@ obj = {
   },
   address: 'JavaScript'
 };
-console.log(obj)
+console.log(obj);
+// Classe define as caracteristicas de um objeto.
+function Book(title, pages, author) {
+  this.title = title;
+  this.pages = pages;
+  this.author = author;
+}
+const book = new Book('title', 'pag', 'author');
+console.log(book.title);
+book.title = 'new title';
+console.log(book.title);
