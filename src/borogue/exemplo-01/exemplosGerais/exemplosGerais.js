638125cc22a45f898f65bd789ef18e0b2a9c0afc
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-26 04:50:57 -0400

    Criando cores aleatorias ao clicar no button, usando o evento body.style.background, com Math Random.

---
+++ src/borogue/exemplo-01/exemplosGerais/exemplosGerais.js
@@ -0,0 +1,7 @@
+// Criando Strings sem precisar concatenar valores.
+let book = {
+ name: 'Estrutura de dados com JavaScript'
+};
+console.log(`You are reading ${book.name}.
+amd this new Line.
+and so is this.`)
