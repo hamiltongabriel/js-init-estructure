a9593a2f187ea869adca1d16025acdb0990f1d4e
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-26 05:16:12 -0400

    Criando um Evento de Resposta Quiz.

--- src/borogue/c1/rainbow/Rainbow.js
+++ src/borogue/c1/rainbow/Rainbow.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('button');
+// getElementById -> Encontra no HTML o elemento com o ID Referenciado.
 const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'rebeccapurple', 'violet'];
 function change() {
   document.body.style.background = rainbow[Math.floor(7 * Math.random())];
