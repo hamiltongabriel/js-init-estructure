e90e7174199652348be5c713c2827b65d19633d6
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-27 05:40:27 -0400

    Dados primitivos tem um suposto wrapper que o deixa com props e metodos.

---
+++ src/borogue/c2/String Propriedades e Metodos/Wrapper Objects(Objetos de invólucro)/TiposPrimitivos.js
@@ -0,0 +1,7 @@
+// Tipos primitivos tem Wrapper que parecem com propriedades e metodos de Objetos.
+const name = 'Hamilton'; // declare and assign a variable
+console.log(name);
+name.length; // retrieve the name variable's length property
+console.log(name.length);
+
+name.;
