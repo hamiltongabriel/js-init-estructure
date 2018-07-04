8d678b458a5bf124520ea49b98894c53660d5d83
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-04 06:03:05 -0400

    Quiz

--- src/borogue/quiz/main.js
+++ src/borogue/quiz/main.js
@@ -1,5 +1,21 @@
 alert('Welcome To Quiz!');
-const question = "Qual nome real do Super Man?";
+const questionDefault = 'Qual nome real do ';
+const question = 'Qual nome real do Super Man? ';
 const answer = prompt(question);
 alert(`Você respondeu ${answer}`);
-
+const quiz = [
+  [`${questionDefault}Superman`, 'Clark Kent'],
+  [`${questionDefault}Wonder Woman`, 'Diana Prince'],
+  [`${questionDefault}Batman`, 'Bruce Wayne']
+];
+let score = 0;
+for (const [question, answer] of quiz) {
+  const response = prompt(question);
+  if (response === answer) {
+    alert('Correto!');
+    score++;
+  } else {
+    alert(`Errado! A resposta correta foi ${answer}`);
+  }
+}
+alert(`Jogo encerrado, voce acertou ${score} point${score !== 1 ? 's' : ''}`);
