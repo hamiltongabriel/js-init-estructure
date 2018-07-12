3e74e5496220d6ba2e77899c5764931bccde3f2e
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-12 05:42:19 -0400

    Complementando o Quiz.

--- src/borogue/quiz/main.js
+++ src/borogue/quiz/main.js
@@ -1,21 +1,34 @@
-alert('Welcome To Quiz!');
 const questionDefault = 'Qual nome real do ';
 const question = 'Qual nome real do Super Man? ';
-const answer = prompt(question);
-alert(`Você respondeu ${answer}`);
 const quiz = [
   [`${questionDefault}Superman`, 'Clark Kent'],
   [`${questionDefault}Wonder Woman`, 'Diana Prince'],
   [`${questionDefault}Batman`, 'Bruce Wayne']
 ];
-let score = 0;
-for (const [question, answer] of quiz) {
-  const response = prompt(question);
-  if (response === answer) {
-    alert('Correto!');
+function start(quiz) {
+  let score = 0;
+  function ask(question) {
+    return prompt(question);
+  }
+  function check(response, answer) {
+    if (response !== answer) {
+      return alert(`Errado! A resposta correta foi ${answer}`);
+    }
+    alert('Correto');
     score++;
-  } else {
-    alert(`Errado! A resposta correta foi ${answer}`);
+  }
+  for (const [question, answer] of quiz) {
+    const response = ask(question);
+    check(response, answer);
+  }
+  gameOver();
+  function gameOver() {
+    acertoTodas();
+    alert(`Jogo encerrado, voce acertou ${score} pergunta${score !== 1 ? 's' : ''}`);
+  }
+  function acertoTodas() {
+    console.log(score === quiz.length ? `${score = 'todas as'} ` : '');
   }
 }
-alert(`Jogo encerrado, voce acertou ${score} point${score !== 1 ? 's' : ''}`);
+
+start(quiz);
