43d6bdd12dd65c0a50ceed5dde58c99dfd3b24f1
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-20 05:12:04 -0400

    While And Do While And For

--- src/structureData/conditions/conditions.js
+++ src/structureData/conditions/conditions.js
@@ -29,10 +29,12 @@ switch (styleValue) {
   case 1:
     console.log('1');
     break;
-    case 2:
+  case 2:
     console.log('2');
     break;
-    case 3:
+  case 3:
     console.log('3');
     break;
+  default:
+    console.log('Nenhum dos valores');
 }
---
+++ src/structureData/loop/loop.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+// While
+let x = 0;
+while (x < 10) {
+  console.log(x);
+  x++;
+}
+// do While
+let z = 0;
+do {
+  console.log(z);
+  z++;
+} while (z < 10);
