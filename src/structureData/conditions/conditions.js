c3a4272a8c6fca96583514dd2732084289b9f2c8
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-20 04:35:46 -0400

    Condicoes e Operador Ternario

---
+++ src/structureData/conditions/conditions.js
@@ -0,0 +1,14 @@
+const valor = 1;
+if (valor === 1) {
+  console.log(`Numero e igual a ${valor}`);
+} else {
+  console.log(`Numero e diferente de 1, valor atual: ${valor}`);
+}
+// Usando Operadores ternarios
+
+function operatorTernary() {
+  valor === 1 ? console.log('sim') : console.log('nao');
+  console.log(valor);
+
+}
+this.operatorTernary();
