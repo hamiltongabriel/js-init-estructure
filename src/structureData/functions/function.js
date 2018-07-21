5d0f2ce91ec0e08fb710d52d30f69b823444fb36
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-21 05:10:13 -0400

    .

---
+++ src/structureData/functions/function.js
@@ -0,0 +1,10 @@
+// Sintaxe basica de uma funcao
+function emitConsole() {
+  console.log('Hello! Function');
+}
+emitConsole();
+
+function recebeArgumentos(texto) {
+  console.log(texto);
+}
+recebeArgumentos('Testando');
