fc724e3fb0abaea2545878a76288e235ffbbc110
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-25 05:57:12 -0400

    Loops

---
+++ src/dataStructures/repetitionConstructs/loopFor/loopFor.js
@@ -0,0 +1,15 @@
+var number = 1;
+var sum = 0;
+for (var number = 1; number < 11; number ++){
+    sum += number;
+console.log(sum)
+
+}
+console.log(sum)
+// Acessando Array com For
+var numbers = [3, 7, 12, 22, 100];
+var sum = 0;
+for (var i = 0; i < numbers.length; ++i){
+    sum += numbers[i]
+}
+console.log(sum);
