d43b192dcd33c3cbc3fdbda80c254a720ef404f8
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-31 19:16:36 -0400

    Pop Push Shif E Unshift 'usando os metodos'

---
+++ src/ninjex/c3/PopPushShiftAndUnshift/main.js
@@ -0,0 +1,9 @@
+const avengers = ['Captain America', 'Iron Man', 'Thor'];
+// Retorna o item e remove o ultimo item da Array...
+console.log(avengers.pop());
+// Retorn o primeiro item e remove o item da Array.
+console.log(avengers.shift());
+// Adiciona um novo valor no final do Array e retorna o tamanho da variavel..
+console.log(avengers.push('Thor'));
+// Adiciona um novo valor no inicio do Array e retorna o tamanho da variavel...
+avengers.unshift('Captain American');
