baf2c35876b373967387885477bc00504a83d6c2
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-31 18:45:08 -0400

    Desestruturação de Arrays

--- src/ninjex/c3/Arrays/main.js
+++ src/ninjex/c3/Arrays/main.js
@@ -5,3 +5,13 @@ console.log(heroes[0]);
 heroes[0] = 'Batman';
 heroes[1] = 'Wonder Woman';
 heroes[5] = 'Aquaman';
+heroes[2] = 'Flash';
+
+// Creating Array Literals
+const avengers = ['Captain America', 'Iron Man', 'Thor', 'Hulk'];
+// Removendo valores do Array.
+// delete substitui o valor atual por undefined.
+console.log(delete avengers[3]);
+/* Pode causar problema devido ao tamanho permanecer o mesmo, porem com valor indefinido. */
+// Exemplo 
+console.log(avengers[3]);
---
+++ src/ninjex/c3/desctruturingArrays/main.js
@@ -0,0 +1,8 @@
+// Desestruturando o Array
+let [x, y] = [1, 2];
+console.log(x);
+console.log(y);
+// Trocando os valores das variaveis utilizando desestruturação.
+[x, y] = [y, x];
+console.log(x);
+console.log(y);
