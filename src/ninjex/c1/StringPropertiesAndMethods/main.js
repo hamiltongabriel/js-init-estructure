d191090ce0da35e160c4342e4bd32edc2c007905
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-30 23:07:43 -0400

    String Properties and Methods

---
+++ src/ninjex/c1/StringPropertiesAndMethods/main.js
@@ -0,0 +1,14 @@
+const name = 'hamilton';
+console.log(name.length);
+console.log(name['length']); // em certas ocasioes colchetes é preferivel.
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.charAt(1));
+console.log(name.indexOf('m'));
+console.log(name.lastIndexOf('a'));
+console.log(name.includes('a'));
+console.log(name.startsWith('h'));
+console.log(name.endsWith('n'));
+console.log('JavaScript'.concat(' ', 'Ninja'));
+console.log('    Hello World         '.trim());
+console.log('Hello '.repeat(2));
--- src/ninjex/c1/declaringAndAssign/main.js
+++ src/ninjex/c1/declaringAndAssign/main.js
@@ -1,9 +1,9 @@
 const name = 'Hamilton';
 const score = 0;
 console.log(name);
-let x = 3, 
-y = 5, 
-z = 10;
+let x = 3,
+  y = 5,
+  z = 10;
 
 
 // Mutação de Objeto
@@ -26,9 +26,23 @@ console.log(a);
 
 // Variável global sendo sobrescrita.
 let b = 2;
-{ b = 4; console.log(b) };
+{ b = 4; console.log(b); }
 console.log(b);
 
 // Criando uma váriavel global dentro de um bloco.
-{ c = 5; console.log(c);}
-console.log(c)
+{ c = 5; console.log(c); }
+console.log(c);
+
+// Atribuição direta ou atribuição por referencia.
+// Atribuição direta
+const e = 1;
+let d = e;
+f = 2;
+// Atribuição por referencia.
+const c = { value: 1 };
+let d = c;
+d.value = 2; // c.value = 2, d.value = 2;
+
+// acessando propriedades de um dado primitivo.
+const myName = 'Hamilton';
+console.log(myName.length);
