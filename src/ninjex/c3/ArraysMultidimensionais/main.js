9a7a8202fcfcef88a75f71c26545967fdddf2923
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-01 19:05:26 -0400

    Junção de Arrays.

--- src/ninjex/c3/ArraysMultidimensionais/main.js
+++ src/ninjex/c3/ArraysMultidimensionais/main.js
@@ -6,3 +6,13 @@ console.log(coordinates[0][0]);
 console.log(coordinates[1][0]);
 console.log(coordinates[0][1]);
 console.log(coordinates[1][1]);
+
+// Spread pode ser usado para nivelar Arrays multidimensionais.
+// Exemplo de matriz Achatada(Multidimensional).
+const summer = ['Jun', 'Jul', 'Ago'];
+const winter = ['Dec', 'Jan', 'Fev'];
+const nested = [ summer, winter ];
+console.log(nested);
+// Realizando a junção dos arrays
+const flat = [...summer, ...winter];
+console.log(flat);
