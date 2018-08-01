62635d7e471ea29ddaf1ec981db190806ba8a202
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-01 04:22:44 -0400

    Usando Sort Para Ordenação

--- src/ninjex/c3/Sort/main.js
+++ src/ninjex/c3/Sort/main.js
@@ -1,4 +1,5 @@
 const avengers = ['Captain America', 'Iron Man', 'Hamilton', 'Thor'];
 // Usamos Sort para Ordenar a Ordem de um Array permanentemente
 console.log(avengers.sort());
-
+// Para ordenação numerica é necessário retornar uma função para o Sort.
+// Pois ele ordena pela primeira casa singular.
