68d632f846863ea3cb1498779718215e09a9c1e6
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-01 04:39:43 -0400

    Usando matriz dentro de matriz para definição de coordenada

---
+++ src/ninjex/c3/ArraysMultidimensionais/main.js
@@ -0,0 +1,8 @@
+// Usamos matriz de matrizes para criação de coordenadas.
+const coordinates = [[1, 3], [4, 2]];
+console.log(coordinates);
+// Para acessar primeiramente acessamos a matriz externa, e logo após a interna
+console.log(coordinates[0][0]);
+console.log(coordinates[1][0]);
+console.log(coordinates[0][1]);
+console.log(coordinates[1][1]);
