f1dd0862aba5a2e2ea73a8c8281a025714468052
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-06 05:40:21 -0400

    Usando Maps e For-Of para iteração dos arrays.

--- src/quiz/main.js
+++ src/quiz/main.js
@@ -1,4 +1,20 @@
 alert('Bem vindo ao Quiz');
-const question = "What is Superman's real name?";
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+// Cria 1 new Map
+const quiz = new Map([
+  ['Qual o nome real do superman?', 'Clark Kent'],
+  ['Qual o nome real do Wonder Woman?', 'Diana Prince'],
+  ['Qual o nome real do batamn?', 'Bruce Wayne']
+]);
+let score = 0;
+
+// Realizando um for-of, pegando a entranda do new map.
+for (const [question, answer] of quiz.entries()) {
+  const response = prompt(question);
+  if (response === answer) {
+    alert('Correto');
+    score++;
+  }else {
+    alert(`Errado! A resposta correta é ${answer}`);
+  }
+}
+alert(`Game Over, você acertou ${score} point${score > 1 ? 's' : ''}`);
