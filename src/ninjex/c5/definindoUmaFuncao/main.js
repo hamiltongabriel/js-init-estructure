930aafb6c64cd6821503de61d4d76802471247f4
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-10 05:46:39 -0400

    Function

--- src/ninjex/c5/definindoUmaFuncao/main.js
+++ src/ninjex/c5/definindoUmaFuncao/main.js
@@ -15,3 +15,26 @@ const goodBye = function bye() {
   console.log('Goodbye World!');
 };
 goodBye();
+
+// Toda função tem um nome
+console.log(goodBye.nome);
+console.log(goodbye.nome);
+console.log(hello.nome);
+
+// Return function
+function howdy() {
+  return 'Howdy World!';
+}
+const message = howdy();
+console.log(message);
+
+// Parametros e Argumentos
+function square(parametro) {
+  return console.log(parametro * parametro);
+}
+const argumento = 4.5;
+square(argumento);
+function mean(a, b, c) {
+  return console.log((a + b + c)) / 3;
+}
+mean(1, 3, 6);
