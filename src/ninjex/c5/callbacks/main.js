c097e6e8da9a34e94ef7c1a31a3492ec7fe520fe
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-13 23:48:36 -0400

    Call Back

---
+++ src/ninjex/c5/callbacks/main.js
@@ -0,0 +1,15 @@
+// Calback é uma função passada como parametro para outra.
+// função sing que recebe uma musica como parametro.
+function sing(song, callback) {
+  console.log(`Eu te ${song}.`);
+  if (typeof (callback) === 'function') {
+    callback();
+  }
+}
+// Callback com função
+sing('Amo', dance);
+function dance() {
+  console.log('Mariana');
+}
+// Callback com função anonima.
+sing('Amo', () => { console.log('Hamilton'); });
